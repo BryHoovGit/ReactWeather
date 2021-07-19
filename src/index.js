c234a7a8f434bb87d1b14fa89b668a2013c1a964
Bryce Hoover <bryhoover11@gmail.com>
2021-07-19 14:52:04 -0500

    Basic error handling

--- src/index.js
+++ src/index.js
@@ -6,23 +6,31 @@ class App extends React.Component {
         super(props);
 
         //Initializing state
-        this.state = {lat: null};
+        this.state = { lat: null, errorMessage: '' };
 
         //Gets user's current location
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({ lat: position.coords.latitude})
             },
-            (err) => console.log(err)
+            (err) => 
+            {
+                this.setState({ errorMessage: err.message })
+            }
         );
     }
 
     render() {
-        return (
-            <div>
-                Latitude: {this.state.lat}
-            </div>
-        )
+        //No lat, has error
+        if (this.state.errorMessage && !this.state.lat) {
+           return <div>Error: {this.state.errorMessage}</div>; 
+        }
+        // Has lat, no error
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+        //No lat, no error
+        return <div>Loading!</div>;
     }
 }
 

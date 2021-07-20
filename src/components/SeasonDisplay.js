e0dacbbfe49a74ec975172795932aa9c295789f3
Bryce Hoover <bryhoover11@gmail.com>
2021-07-20 14:31:01 -0500

    Added icons and logic for icons  to SeasonDisplay component

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -8,12 +8,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    
+    const text = season === 'winter' ? "Burr, it's chilly!" : "Let's hit the beach!";
+    const icon = season === 'winter' ? "snowflake" : "sun";
+
     return (
         <div>
-            Season Display
+            <i className={`${icon} icon`} />
+            <h1>{text}</h1>
+            <i className={`${icon} icon`} />
         </div>
-    )
+    );
 }
 
 export default SeasonDisplay

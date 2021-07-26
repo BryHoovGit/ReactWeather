6bbe6f25ed9f67026867a7c0f7cf48e403137cec
Bryce Hoover <bryhoover11@gmail.com>
2021-07-26 16:49:29 -0500

    Refactor

--- src/index.js
+++ src/index.js
@@ -22,18 +22,25 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    // remove conditionals and fix multiple render method issues when expanding or changing codebase 
+    
+    renderContent() {
         //No lat, has error
         if (this.state.errorMessage && !this.state.lat) {
-           return <div>Error: {this.state.errorMessage}</div>; 
-        }
-        // Has lat, no error
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />
-        }
-        //No lat, no error
-        return <Spinner message="Please accept location request"/>;
+            return <div>Error: {this.state.errorMessage}</div>; 
+         }
+         // Has lat, no error
+         if (!this.state.errorMessage && this.state.lat) {
+             return <SeasonDisplay lat={this.state.lat} />
+         }
+         //No lat, no error
+         return <Spinner message="Please accept location request"/>;
     }
+
+    render() {
+        return (
+        <div>{this.renderContent()}</div>
+    )}
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
